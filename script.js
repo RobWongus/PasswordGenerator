bd1e349263653e976f6cc5aa4182a89169879c68
Robert Wongus <rob_wo@yahoo.com>
2020-02-14 22:58:15 -0500

    changed variables

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 let welcome = alert("Welcome to password generator!")
-let question1 = prompt("Pick a number between 0 and 128.")
 let characters = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 let numbers = ("0123456789")
-let numberOfCharacters = ("8")
+let numberOfCharacters = i > 8; i < 128;
 let symbols = ("!@#$%^&*+?")
 
-let charNum = document.getElementById("charNum");
-let numBox = document.getElementById("numBox");
-let symBox = document.getElementById("sym");
-let enter = document.getElementById("enter");
-let yourPw = document.getElementById("yourPw");
+let randomFunction = {
+  characters: getRandomCharacters,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
 
-submit.addEventListener("click", function(e){
-
-  let characters = charcters;
-  (btn) ? characters += numbers += symbols : ""; 
-});
+submit.addEventListener("click", () =>{
 
 
-function writePassword(length, characters) {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword(characters, numbers, symbols) {
+  let password = generate(characters + numbers + symbols);
+  let textCount = numberOfCharacters
+  let passwordText = document.querySelector("#password");
 
-  for (let i = 0; i<length; i++)
+  for (let i = 0; i < length; i++)
     pwd += characters.charAt(Math.floor(Math.random() * characters.length));
 
   }
@@ -46,4 +43,4 @@ var generateBtn = document.querySelector("#generate");
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
-
+}
