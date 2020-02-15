7aa3e75677c9e1f53d9b8dd0a9584f8bbe7a26d3
Robert Wongus <rob_wo@yahoo.com>
2020-02-14 19:57:36 -0500

    made simple

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-let characterNumber = prompt("How many characters do you want for your password?")
-
-if (characterNumber > 8 ) {
-   
-    console.log(characterNumber)
-}else{
-    characterNumber = prompt("Pick a number > 8 and < 128");
-}
-
-
-
-
-
-
-
-let generateBtn = document.querySelector("#generate");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
