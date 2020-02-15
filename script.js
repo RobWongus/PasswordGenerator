f5aaf5adc76e8f0632285a737f335f0b971994a3
Robert Wongus <rob_wo@yahoo.com>
2020-02-14 21:30:11 -0500

    added to ReadME

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+let welcome = alert("Welcome to password generator!")
+let question1 = prompt("Pick a number between 0 and 128.")
+let characters = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+let numbers = ("0123456789")
+let numberOfCharacters = ("8")
+let symbols = ("!@#$%^&*+?")
 
-// Write password to the #password input
-function writePassword() {
+let charNum = document.getElementById("charNum");
+let numBox = document.getElementById("numBox");
+let symBox = document.getElementById("sym");
+let enter = document.getElementById("enter");
+let yourPw = document.getElementById("yourPw");
+
+submit.addEventListener("click", function(e){
+
+  let characters = charcters;
+  (btn) ? characters += numbers += symbols : ""; 
+});
+
+
+function writePassword(length, characters) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  for (let i = 0; i<length; i++)
+    pwd += characters.charAt(Math.floor(Math.random() * characters.length));
 
-}
+  }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword(length, characters) {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   for (let i = 0; i<length; i++)
+//     pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+
+//   }
+
+//   passwordText.value = password;
+
+
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
